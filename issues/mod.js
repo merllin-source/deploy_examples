11d66e582d6d6025edcadc0a0107be8f6fb03a81
merllin-source <52001177+merllin-source@users.noreply.github.com>
2021-04-02 00:57:03 +0800

    Update mod.js

--- issues/mod.js
+++ issues/mod.js
@@ -1,6 +1,9 @@
-addEventListener("fetch", (event) => {
-  const response = new Response("Hello World!", {
-    headers: { "content-type": "text/plain" },
-  });
-  event.respondWith(response);
-})
+import {
+  json,
+  serve,
+  validateRequest,
+} from "https://deno.land/x/sift@0.1.7/mod.ts";
+
+serve({
+  "/quotes": handleQuotes,
+});
