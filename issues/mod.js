0d24fcab0651aeb0cffa7f5fc129cef04795dfa9
merllin-source <52001177+merllin-source@users.noreply.github.com>
2021-04-02 00:51:29 +0800

    Update mod.js

--- issues/mod.js
+++ issues/mod.js
@@ -1,36 +1,39 @@
-import {
-  json,
-  serve,
-  validateRequest,
-} from "https://deno.land/x/sift@0.1.7/mod.ts";
-
-serve({
-  "/quotes": handleQuotes,
-});
-
-// To get started, let's just use a global array of quotes.
-const quotes = [
-  {
-    quote: "Those who can imagine anything, can create the impossible.",
-    author: "Alan Turing",
-  },
-  {
-    quote: "Any sufficiently advanced technology is equivalent to magic.",
-    author: "Arthur C. Clarke",
-  },
-];
-
-async function handleQuotes(request: Request) {
-  // Make sure the request is a GET request.
-  const { error } = await validateRequest(request, {
-    GET: {},
-  });
-  // validateRequest populates the error if the request doesn't meet
-  // the schema we defined.
-  if (error) {
-    return json({ error: error.message }, { status: error.status });
+async function queryFuana(
+  query: string,
+  variables: { [key: string]: unknown },
+): Promise<{
+  data?: any;
+  error?: any;
+}> {
+  // Grab the secret from the environment.
+  const token = Deno.env.get("FAUNA_SECRET");
+  if (!token) {
+    throw new Error("environment variable FAUNA_SECRET not set");
   }
 
-  // Return all the quotes.
-  return json({ quotes });
+  try {
+    // Make a POST request to fauna's graphql endpoint with body being
+    // the query and its variables.
+    const res = await fetch("https://graphql.fauna.com/graphql", {
+      method: "POST",
+      headers: {
+        authorization: `Bearer ${token}`,
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        query,
+        variables,
+      }),
+    });
+
+    const { data, errors } = await res.json();
+    if (errors) {
+      // Return the first error if there are any.
+      return { data, error: errors[0] };
+    }
+
+    return { data };
+  } catch (error) {
+    return { error };
+  }
 }
